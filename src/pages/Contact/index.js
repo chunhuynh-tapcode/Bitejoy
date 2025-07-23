48b0c54891e288ea11701a04b54a77dbe178b4b9
chunhuynh-tapcode <huynhdinhtrung2311@gmail.com>
2025-07-23 14:50:27 +0700

    Second Commit

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,11 @@
+import "./Contact.module.scss"
+
+function Contact() {
+    return (
+        <div className="Contact">
+            <h1>Welcome to the Contact Page</h1>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ src/pages/Menu/index.js
@@ -0,0 +1,11 @@
+import "./Menu.module.scss"
+
+function Menu() {
+    return (
+        <div className="Menu">
+            <h1>Welcome to the Menu Page</h1>
+        </div>
+    );
+}
+
+export default Menu;
